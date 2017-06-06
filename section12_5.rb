ceb91ffb2dd3fd1844363303ac5a583542a00150
Asami Niino <asami.niino@luxiar.com>
2017-06-06 16:02:00 +0900

    練習問題（５）

---
+++ section12_5.rb
@@ -0,0 +1,17 @@
+def prime?(num)
+  case num
+  when 1
+    false
+  when 2
+    true
+  else
+    (2..(num-1)).each do |index|
+      if num % index == 0
+      return false
+      end
+  end
+  return true
+  end
+end
+
+p prime?(6)
