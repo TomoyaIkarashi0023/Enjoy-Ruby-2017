b135af97f6ba7d5630e8a42070dd0a12a57751a1
Asami Niino <asami.niino@luxiar.com>
2017-06-08 11:48:25 +0900

    ハッシュクラス練習問題（３）

---
+++ section15_3.rb
@@ -0,0 +1,6 @@
+wday = {sunday: "日曜日", monday: "月曜日", tuesday: "火曜日", wednesday: "水曜日",
+  thursday: "木曜日", friday: "金曜日", saturday: "土曜日" }
+
+wday.each do |key, value|
+  p "#{key}は#{value}のことです。"
+end
